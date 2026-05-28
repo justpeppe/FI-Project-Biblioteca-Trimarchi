be7680bcc1fc96eeafcfaf750f3bae1e74c2360d
justpeppe <trimarchig2002@gmail.com>
2026-05-28 22:43:52 +0200

    fix

--- libri.h
+++ libri.h
@@ -2,25 +2,25 @@
 #define LIBRI_H
 #include "libro.h"
 
-struct libri {
+struct libri
+{
     libro l;
-    struct libri* successivo;
+    struct libri *successivo;
 };
 
-typedef struct ibri* libri;
+typedef struct libri *libri;
 
+int crea_lista_libri(libri *lista);
 
-int crea_lista_libri(libri* lista);
-
-int inserisci_nuovo_libro(libri* lista, char titolo[], char nome_autore[], char cognome_autore[]);
+int inserisci_nuovo_libro(libri *lista, char titolo[], char nome_autore[], char cognome_autore[]);
 
 libro cerca_libro_nella_lista(libri lista, char titolo_chiave[]);
 
-int modifica_libro_nella_lista(libri* lista, char titolo_attuale[], char nuovo_titolo[], char nuovo_nome_autore[], char nuovo_cognome_autore[]);
+int modifica_libro_nella_lista(libri *lista, char titolo_attuale[], char nuovo_titolo[], char nuovo_nome_autore[], char nuovo_cognome_autore[]);
 
-int cancella_libro_dalla_lista(libri* lista, char titolo_chiave[]);
+int cancella_libro_dalla_lista(libri *lista, char titolo_chiave[]);
 
-int distruggi_lista_libri(libri* lista);
+int distruggi_lista_libri(libri *lista);
 
 void stampa_lista_libri(libri lista);
 
